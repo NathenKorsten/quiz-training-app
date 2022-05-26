859deffca13c8f7baf694be3647ae69112c5312d
Nathen <nktheastranout@gmail.com>
2022-05-26 13:23:20 +0200

    Removed Question Model

--- src/app/models/question.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Question } from './question.model';
-
-describe('Question', () => {
-  it('should create an instance', () => {
-    expect(new Question(0, [])).toBeTruthy();
-  });
-});
--- src/app/models/question.model.ts
+++
@@ -1,9 +0,0 @@
-export class Question {
-  questionID: number;
-  options: any[];
-
-  constructor(questionID: number, options: any[]) {
-    this.questionID = questionID;
-    this.options = options;
-  }
-}
